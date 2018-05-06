5949442a01d450d7db35cf261b4a2f72476b36be
Luc Patiny <luc@patiny.com>
2018-05-06 15:20:52 +0200

    working on arduino test program

--- arduino/test/testOut/testOut.ino
+++ arduino/test/testOut/testOut.ino
@@ -1,58 +1,29 @@
-/*
-     The purpose of this code is to test the beemos-counter board.
-     The board has 16 infrared emitters/receptors controlled by 4
-     analog pins via a multiplexer.
-*/
-
-byte out[] = {1, 0, 7, 2, 6, 3, 5, 4, 8, 9, 15, 10, 14, 11, 13, 12};
-// byte out[] = {1, 0};
-#define OUT0 A0
-#define OUT1 A1
-#define OUT2 A2
-#define OUT3 A3
-
-byte in[] = {6, 7, 0, 5, 1, 4, 2, 3, 8, 9, 14, 15, 12, 13, 10, 11};
-
-#define IN0 5
-#define IN1 9
-#define IN2 8
-#define IN3 6
-#define IN_EN 10
-#define MUX A4
 int muxValue = 0;
 
+
+int current[16];
+
+
 void setup() {
-  pinMode(IN0, OUTPUT);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(OUT0, OUTPUT);
-  pinMode(OUT1, OUTPUT);
-  pinMode(OUT2, OUTPUT);
-  pinMode(OUT3, OUTPUT);
-  pinMode(MUX, INPUT_PULLUP);
   Serial.begin(9600);
-  pinMode(IN_EN, OUTPUT);
-  digitalWrite(IN_EN, LOW);
+  initCounter();
+  initMax();
 }
+
 
 
 // the loop function runs over and over again forever
 void loop() {
   // Iterating over infrared emitters
   while (true) {
-    for (byte i = 0; i < sizeof(out); i++) {
-      setOutput(out[i]);
-      setInput(in[i]);
-      delay(1);
-      muxValue = analogRead(MUX);
 
-      Serial.print(muxValue);
-      Serial.print(" ");
-      delay(10);
-    }
-    delay(100);
-    Serial.println();
+
+    updateCounter();
+    printCounter();
+    updateMax();
+
+
+    delay(500);
   }
 
 
@@ -71,50 +42,3 @@ void switchBlink() {
   }
 }
 
-// Enable Output pins based on binary value of input i.
-// This enables infrared emitter number i.
-void setOutput(byte i) {
-  if (i & 0b0001) {
-    digitalWrite(OUT0, HIGH);
-  } else {
-    digitalWrite(OUT0, LOW);
-  }
-  if (i & 0b0010) {
-    digitalWrite(OUT1, HIGH);
-  } else {
-    digitalWrite(OUT1, LOW);
-  }
-  if (i & 0b0100) {
-    digitalWrite(OUT2, HIGH);
-  } else {
-    digitalWrite(OUT2, LOW);
-  }
-  if (i & 0b1000) {
-    digitalWrite(OUT3, HIGH);
-  } else {
-    digitalWrite(OUT3, LOW);
-  }
-}
-
-void setInput(byte i) {
-  if (i & 0b0001) {
-    digitalWrite(IN0, HIGH);
-  } else {
-    digitalWrite(IN0, LOW);
-  }
-  if (i & 0b0010) {
-    digitalWrite(IN1, HIGH);
-  } else {
-    digitalWrite(IN1, LOW);
-  }
-  if (i & 0b0100) {
-    digitalWrite(IN2, HIGH);
-  } else {
-    digitalWrite(IN2, LOW);
-  }
-  if (i & 0b1000) {
-    digitalWrite(IN3, HIGH);
-  } else {
-    digitalWrite(IN3, LOW);
-  }
-}
