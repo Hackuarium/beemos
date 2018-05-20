a6094d1aa79fe5b3bb2f74be38014c0c37fa5d00
Luc Patiny <luc@patiny.com>
2018-05-20 10:46:07 +0200

    master-simple: add test for wake-up i2c slave

---
+++ master-simple/arduino/tests/wakeupFromWiere/wakeupFromWiere.ino
@@ -0,0 +1,18 @@
+#include <Wire.h>
+
+void setup() {
+  pinMode(13, OUTPUT);
+  Wire.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+  delay(1000);
+  Wire.requestFrom(55, 1);
+  byte value = Wire.read();
+  Serial.println(value);
+  
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+}
